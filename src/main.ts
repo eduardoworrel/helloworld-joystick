f6959418aa61898463d136307a3dfd3115276ae5
eduardoworrel <eduardo.worrel@gmail.com>
2022-04-22 00:30:38 -0300

    desacoplando principal do html

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
 import Move from './Move';
-const principal = document.getElementById('p') as HTMLElement;
+
+const real = document.querySelector('#realcontent') as HTMLElement;
+if(!real) throw new Error('O elemento "realcontent" é obrigatório')
+
+const principal = document.createElement('div') as HTMLElement;
+principal.setAttribute('style', `
+            position:absolute;
+            width: 150px; 
+            height: 150px;
+            background-image: url('https://i.pinimg.com/originals/a0/26/1b/a0261b885cfba5a65c675c33327acf5a.png');
+            background-size: 100%;
+            top: 60px;
+            left: 500px;        
+`);
+real.append(principal);
+
 Move.attach(principal);
